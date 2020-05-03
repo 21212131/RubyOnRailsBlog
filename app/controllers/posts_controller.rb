280e2dd10690b3736f6af008fb47c1ca75fbceb5
21212131 <ziz059@ucsd.edu>
2020-05-03 01:45:10 -0700

    Modified message function and top function bar

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,7 @@ class PostsController < ApplicationController
   # GET /posts.json
   def index
     @posts = Post.all
+    @post = Post.new
   end
 
   # GET /posts/1
@@ -28,13 +29,16 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
+        # format.html { redirect_to @posts, notice: 'Post was successfully created.' }
+        format.html { redirect_to(root_path)}
         format.json { render :show, status: :created, location: @post }
+
       else
         format.html { render :new }
         format.json { render json: @post.errors, status: :unprocessable_entity }
       end
     end
+    # redirect_to("posts")
   end
 
   # PATCH/PUT /posts/1
